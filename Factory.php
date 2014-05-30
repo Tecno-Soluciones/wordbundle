db7ab0d96eb48fdbf186843aeb7d93c80b4c92ed
Lerny <lbetancourt@tecnosoluciones.com>
2014-05-30 16:30:05 -0430

    Correcciones

--- Factory.php
+++ Factory.php
@@ -2,7 +2,7 @@
 
 namespace Tecno\WordBundle;
 
-use Symfony\Component\HttpFoundation\StreamedResponse;
+use PhpOffice\PhpWord\PhpWord;
 
 /**
  * Factory for PHPWord objects, StreamedResponse, and PHPWord_Writer_IWriter.
@@ -13,7 +13,7 @@ class Factory
 {
     private $phpWordIO;
 
-    public function __construct($phpWordIO = '\PHPWord_IOFactory')
+    public function __construct($phpWordIO = 'IOFactory::createWriter')
     {
         $this->phpWordIO = $phpWordIO;
     }
@@ -22,51 +22,16 @@ class Factory
      *
      * @param string $filename
      *
-     * @return \PHPWord
+     * @return PhpWord
      */
     public function createPHPWordObject($filename =  null)
     {
         if (null == $filename) {
-            $phpWordObject = new \PHPWord();
+            $phpWordObject = new PhpWord();
 
             return $phpWordObject;
         }
 
         return call_user_func(array($this->phpWordIO, 'load'), $filename);
-    }
-
-    /**
-     * Create a writer given the PHPWordObject and the type,
-     *   the type coul be one of PHPWord_IOFactory::$_autoResolveClasses
-     *
-     * @param \PHPWord $phpWordObject
-     * @param string    $type
-     *
-     *
-     * @return \PHPWord_Writer_IWriter
-     */
-    public function createWriter(\PHPWord $phpWordObject, $type = 'Word5')
-    {
-        return call_user_func(array($this->phpWordIO, 'createWriter'), $phpWordObject, $type);
-    }
-
-    /**
-     * Stream the file as Response.
-     *
-     * @param \PHPWord_Writer_IWriter $writer
-     * @param int                      $status
-     * @param array                    $headers
-     *
-     * @return StreamedResponse
-     */
-    public function createStreamedResponse(\PHPWord_Writer_IWriter $writer, $status = 200, $headers = array())
-    {
-        return new StreamedResponse(
-            function () use ($writer) {
-                $writer->save('php://output');
-            },
-            $status,
-            $headers
-        );
     }
 }
